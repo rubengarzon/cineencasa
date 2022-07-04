20e64aecf6664d634c89c480198f63c73354c87e
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-07-04 13:25:42 +0200

    ruta y enlace serie modificados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { PeliculasComponent } from './componentes/peliculas/peliculas.component';
 import { SeriesComponent } from './componentes/series/series.component';
 import { PeliculaComponent } from './componentes/peliculas/pelicula/pelicula.component';
+import { SerieComponent } from './componentes/series/serie/serie.component';
 
 const routes: Routes = [
   { path: 'peliculas', component: PeliculasComponent },
   { path: 'series', component: SeriesComponent },
   { path: 'pelicula/:id', component: PeliculaComponent },
-  { path: 'serie/:id', component: PeliculaComponent },
+  { path: 'serie/:id', component: SerieComponent },
   { path: '**', redirectTo: '/peliculas', pathMatch: 'full' },
 ];
 
