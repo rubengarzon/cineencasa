e3e5bcda100fcf99ba5c8a58686327cd9b459b05
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-07-04 13:41:24 +0200

    serie interface añadido

--- src/app/componentes/series/serie/serie.component.ts
+++ src/app/componentes/series/serie/serie.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-serie',
   templateUrl: './serie.component.html',
-  styleUrls: ['./serie.component.sass']
+  styleUrls: ['./serie.component.sass'],
 })
 export class SerieComponent implements OnInit {
-
-  constructor() { }
+  constructor(private ruta: ActivatedRoute) {}
 
   ngOnInit(): void {
+    console.log(this.ruta.snapshot.params['id']);
   }
-
 }
