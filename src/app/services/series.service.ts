95c824bb65ed7ce1ae9db97605c5b2255fad4dae
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-07-04 13:56:32 +0200

    metodo obtener detalle serie añadido

--- src/app/services/series.service.ts
+++ src/app/services/series.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { Series } from '../interfaces/seriespopulares.interface';
+import { Serie, Series } from '../interfaces/seriespopulares.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +13,7 @@ export class SeriesService {
     'https://api.themoviedb.org/3/tv/popular?api_key=7d6cebd7375363a80d7b3517c7036ba6&language=es-ES&page=2';
   url3: string =
     'https://api.themoviedb.org/3/tv/popular?api_key=7d6cebd7375363a80d7b3517c7036ba6&language=es-ES&page=3';
+  url4: string = '';
 
   constructor(private http: HttpClient) {}
 
@@ -24,5 +25,14 @@ export class SeriesService {
   }
   obtenerSeriesPopularesPagina3(): Observable<Series> {
     return this.http.get<Series>(this.url3);
+  }
+  /**
+   * Obtiene el detalle de una serie
+   * @param tv_id string
+   * @returns Observable<Serie>
+   */
+  obtenerDetalleSerie(tv_id: string): Observable<Serie> {
+    this.url4 = `https://api.themoviedb.org/3/tv/${tv_id}?api_key=7d6cebd7375363a80d7b3517c7036ba6&language=es-ES`;
+    return this.http.get<Serie>(this.url4);
   }
 }
