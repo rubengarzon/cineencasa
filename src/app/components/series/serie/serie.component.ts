85a76b8f7bba53691ea780439f9bd7599acb7f84
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-07-05 12:31:45 +0200

    trailer serie añadido

--- src/app/components/series/serie/serie.component.ts
+++ src/app/components/series/serie/serie.component.ts
@@ -2,7 +2,11 @@ import { Serie } from './../../../interfaces/seriespopulares.interface';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SeriesService } from '../../../services/series.service';
-import { TV } from '../../../interfaces/serie.interface';
+import { Genre, TV } from '../../../interfaces/serie.interface';
+import {
+  Flatrate13,
+  Plataforma,
+} from '../../../interfaces/plataforma.interface';
 
 @Component({
   selector: 'app-serie',
@@ -12,6 +16,12 @@ import { TV } from '../../../interfaces/serie.interface';
 export class SerieComponent implements OnInit {
   id: string = '';
   serie!: TV;
+  urlImagen: string = 'https://image.tmdb.org/t/p/original';
+  generos!: Genre[];
+  plataforma!: Flatrate13[];
+  control: boolean = false;
+  urlYoutube: string = '';
+  key: string = '';
 
   constructor(
     private ruta: ActivatedRoute,
@@ -20,10 +30,24 @@ export class SerieComponent implements OnInit {
 
   ngOnInit(): void {
     this.id = this.ruta.snapshot.params['id'];
+    this.seriesServicio.obtenerDetalleSerie(this.id).subscribe((serie: TV) => {
+      this.serie = serie;
+      this.generos = serie.genres;
+    });
     this.seriesServicio
-      .obtenerDetalleSerie(this.id)
-      .subscribe((serie: TV) => {
-        this.serie = serie;
+      .obtenerPlataforma(this.id)
+      .subscribe((plataforma: Plataforma) => {
+        if (plataforma.results.ES.flatrate != undefined) {
+          this.plataforma = plataforma.results.ES.flatrate;
+          this.control = true;
+        } else {
+          this.control = false;
+        }
       });
+    this.seriesServicio.obtenerVideos(this.id).subscribe((videos) => {
+      this.key = videos.results[0].key;
+      this.urlYoutube = `https://www.youtube.com/embed/${this.key}`;
+      document.querySelector('iframe')?.setAttribute('src', this.urlYoutube);
+    });
   }
 }
--- src/app/services/series.service.ts
+++ src/app/services/series.service.ts
@@ -3,6 +3,8 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Series } from '../interfaces/seriespopulares.interface';
 import { TV } from '../interfaces/serie.interface';
+import { Plataforma } from '../interfaces/plataforma.interface';
+import { Videos } from '../interfaces/video.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -15,6 +17,8 @@ export class SeriesService {
   url3: string =
     'https://api.themoviedb.org/3/tv/popular?api_key=7d6cebd7375363a80d7b3517c7036ba6&language=es-ES&page=3';
   url4: string = '';
+  urlObtenerPlataforma: string = '';
+  urlObtenerVideos: string = '';
 
   constructor(private http: HttpClient) {}
 
@@ -35,5 +39,23 @@ export class SeriesService {
   obtenerDetalleSerie(tv_id: string): Observable<TV> {
     this.url4 = `https://api.themoviedb.org/3/tv/${tv_id}?api_key=7d6cebd7375363a80d7b3517c7036ba6&language=es-ES`;
     return this.http.get<TV>(this.url4);
+  }
+  /**
+   * Obtener la plataforma streaming de una serie
+   * @param id string
+   * @returns observable<Plataforma>
+   */
+  obtenerPlataforma(id: string): Observable<Plataforma> {
+    this.urlObtenerPlataforma = `https://api.themoviedb.org/3/tv/${id}/watch/providers?api_key=7d6cebd7375363a80d7b3517c7036ba6&`;
+    return this.http.get<Plataforma>(this.urlObtenerPlataforma);
+  }
+  /**
+   * Obtener trailer de la serie
+   * @param id string
+   * @returns observable<Videos>
+   */
+  obtenerVideos(id: string): Observable<Videos> {
+    this.urlObtenerVideos = `https://api.themoviedb.org/3/tv/${id}/videos?api_key=7d6cebd7375363a80d7b3517c7036ba6&language=es-ES`;
+    return this.http.get<Videos>(this.urlObtenerVideos);
   }
 }
