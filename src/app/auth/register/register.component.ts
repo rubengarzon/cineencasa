3ad7f0a400e9e5f72bc9203683eed6d2aa1eb531
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-07-07 19:01:56 +0200

    alertas añadidas en el componente registro

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -10,23 +16,39 @@ import { UserService } from 'src/app/services/auth.service';
 })
 export class RegisterComponent implements OnInit {
   formReg: FormGroup;
-  constructor(private userService: UserService, private router: Router) {
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private readonly fb: FormBuilder,
+    private toastr: ToastrService
+  ) {
     this.formReg = new FormGroup({
       email: new FormControl(),
       password: new FormControl(),
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.formReg = this.initForm();
+  }
 
-  onSubmit() {
+  initForm(): FormGroup {
+    return this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onRegister() {
     this.userService
       .register(this.formReg.value)
       .then((response: any) => {
-        console.log(response);
+        this.toastr.info('Te has registrado correctamente');
         this.userService.isLogged = true;
-        this.router.navigate(['/login']);
+        this.router.navigate(['/peliculas']);
       })
-      .catch((error: any) => console.log(error));
+      .catch((error: any) => {
+        this.toastr.error('Se ha producido un error, intentelo de nuevo')
+      });
   }
 }
