e8bb15f729f7be4e30aa351ec62bb700ad0b0404
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-08-22 12:28:35 +0200

    añadido api urls en environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,21 @@ export const environment = {
   firebase: {
     projectId: 'cineenmicasa-2b357',
     appId: '1:978185028876:web:8b601c429976994743b529',
-    databaseURL: 'https://cineenmicasa-2b357-default-rtdb.europe-west1.firebasedatabase.app',
+    databaseURL:
+      'https://cineenmicasa-2b357-default-rtdb.europe-west1.firebasedatabase.app',
     storageBucket: 'cineenmicasa-2b357.appspot.com',
     locationId: 'europe-west',
     apiKey: 'AIzaSyDfe-iC85LUt3li7cWY88ERYKlZJW35rMc',
     authDomain: 'cineenmicasa-2b357.firebaseapp.com',
     messagingSenderId: '978185028876',
   },
-  production: true
+  apiUrls: {
+    moviePopularPage1:
+      'https://api.themoviedb.org/3/movie/popular?api_key=7d6cebd7375363a80d7b3517c7036ba6&language=es-ES',
+    moviePopularPage2:
+      'https://api.themoviedb.org/3/movie/popular?api_key=7d6cebd7375363a80d7b3517c7036ba6&language=es-ES&page=2',
+    moviePopularPage3:
+      'https://api.themoviedb.org/3/movie/popular?api_key=7d6cebd7375363a80d7b3517c7036ba6&language=es-ES&page=3',
+  },
+  production: true,
 };
